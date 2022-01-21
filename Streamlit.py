b8585e8f923917c039eab538a15decd11e2e8acd
visruthcv <64276081+visruthcv@users.noreply.github.com>
2022-01-21 17:10:45 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import sklearn
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from collections import Counter
